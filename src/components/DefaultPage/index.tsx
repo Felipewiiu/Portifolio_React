32dbb1eeb05f595c8cb1b6dab75de583844bd8d2
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-07-11 14:21:57 -0300

    Criando as variáveis css

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+
 import styles from './DefautPage.module.scss';
 
 export default function DefaultPage() {
   return (
-    <h1  className={styles.title}>Pagina padrão</h1>
+    <>
+      <h1>Pagina padrão</h1>
+    </>
   );
 }
