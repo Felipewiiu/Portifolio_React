0dffcd7cec517ecb1425b260ea664af55a20fff8
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-07-12 07:22:38 -0300

    Criando a pagina padrão

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -3,8 +3,9 @@ import styles from './DefautPage.module.scss';
 
 export default function DefaultPage() {
   return (
-    <>
+    <div className={styles.container}>
       <h1>Pagina padrão</h1>
-    </>
+    </div>
+
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DefaultPage from 'components/DefaultPage';
-
 import React from 'react';
 
 export default function AppRoutes() {
