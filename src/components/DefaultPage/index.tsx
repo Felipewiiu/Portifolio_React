6c66bb2d707b11100adc49d24c34bc79b54c4751
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-07-12 22:21:24 -0300

    fix bug01

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,11 +1,13 @@
-
-import Menu from 'components/Menu';
+import Menu from 'components/menu';
 import styles from './DefautPage.module.scss';
 
 export default function DefaultPage() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.container__menu}>
+        <Menu/>
+
+      </div>
     </div>
 
   );
