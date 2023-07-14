5b5ea53df5a2106241c635e8ebebcf520e80098a
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-07-13 22:17:54 -0300

    Implementando a pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styles from './home.module.scss';
+import { ReactComponent as Github } from 'assets/github.svg';
+import { ReactComponent as Instagran } from 'assets/instagran.svg';
+import { ReactComponent as Linkedin } from 'assets/linkedin.svg';
+
 
 export default function Home() {
   return (
-    <div>teste da pagina home</div>
+    <section className={styles.container}>
+      <div className={styles.container__secundary}>
+        <h1 className={styles.container__title}>
+          Front-end developer,
+          focado em resultado e um curioso.
+        </h1>
+        <p>
+          Eu sou Felipe Oliveira, e
+          me especializo em React JS.
+          Acredito que metas bem planejadas
+          te levam a grandes resultados, sigo
+          minha trajetória com o objetivo de evoluir
+          para se tornar um Dev Full-stack.
+        </p>
+        <div className={styles.container__img}>
+          <div className={styles.container__distribution}>
+            <Github />
+            <Instagran />
+            <Linkedin />
+          </div>
+        </div>
+      </div>
+
+    </section>
   );
 }
