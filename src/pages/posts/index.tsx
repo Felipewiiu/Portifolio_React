76c29ee96a0640052851b2c586306bc49ab2b835
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-07-18 22:09:55 -0300

    styles: Criando o estilo do componente card

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,36 +3,6 @@ import styles from './posts.module.scss';
 
 export default function Posts() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.container__title}>
-        Confira o que eu ando aprontando.
-      </h1>
-      <p>
-        Aqui você pode conferir todos os projetos que
-        venho construindo durante o tempo, com o objetivo
-        de praticar os conhecimentos adquiridos nos cursos da Alura.
-      </p>
-      <div className={styles.container__section}>
-        <div className={styles.container__card}>
-          
-        </div>
-        <div className={styles.container__card}>
-          
-        </div>
-        <div className={styles.container__card}>
-          
-        </div>
-        <div className={styles.container__card}>
-          
-        </div>
-        <div className={styles.container__card}>
-          
-        </div>
-        <div className={styles.container__card}>
-          
-        </div>
-
-      </div>
-    </div>
+    <h1>Posts</h1>
   );
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import styles from './projects.module.scss';
 
 export default function Projects() {
   return (
-    <h1>Projetos</h1>
+    <div className={styles.container}>
+      <h1 className={styles.container__title}>
+        Confira o que eu ando aprontando.
+      </h1>
+      <p>
+        Aqui você pode conferir todos os projetos que
+        venho construindo durante o tempo, com o objetivo
+        de praticar os conhecimentos adquiridos nos cursos da Alura.
+      </p>
+      <div className={styles.container__section}>
+
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+        <div className={styles.container__card}>
+          <div className={styles.container__description}>
+            <p>Nome do projeto</p>
+            <button>
+              Ver mais
+            </button>
+          </div>
+        </div>
+       
+        
+
+      </div>
+    </div>
   );
 }
