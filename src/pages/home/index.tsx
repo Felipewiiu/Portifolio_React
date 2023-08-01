389e5f7eb20ba55713fa1e9e760a484d8e5ba183
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-08-01 14:22:50 -0300

    Fix: Ajustando o layout da página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,32 +9,33 @@ import Menu from 'components/menu';
 export default function Home() {
   return (
     <section className={styles.container}>
-      <Menu/>
-      <div className={styles.container__secundary}>
-        <h1 className={styles.container__title}>
-          Front-end developer,
-          focado em resultado e um curioso.
-        </h1>
-        <p>
-          Eu sou Felipe Oliveira, e
-          me especializo em React JS.
-          Acredito que metas bem planejadas
-          te levam a grandes resultados, sigo
-          minha trajetória com o objetivo de evoluir
-          para se tornar um Dev Full-stack.
-        </p>
+      <Menu />
+      <div className={styles.container__outlet}>
+        <div className={styles.container__secundary}>
+          <h1 className={styles.container__title}>
+            Front-end developer,
+            focado em resultado e um curioso.
+          </h1>
+          <p>
+            Eu sou Felipe Oliveira, e
+            me especializo em React JS.
+            Acredito que metas bem planejadas
+            te levam a grandes resultados, sigo
+            minha trajetória com o objetivo de evoluir
+            para se tornar um Dev Full-stack.
+          </p>
 
-      </div>
-      <div className={styles.container__containerImg} >
-        <div className={styles.container__imgProfile}></div>
-        <div className={styles.socialMedia}>
-          <a href="https://github.com/Felipewiiu" target='blank'><Github /></a>
-          <a href="https://www.instagram.com/" target='blank'><Instagran /></a>
-          <a href="https://www.linkedin.com/in/felipe-front-end/" target='blank'><Linkedin /></a>
         </div>
-
-
+        <div className={styles.container__containerImg} >
+          <div className={styles.container__imgProfile}></div>
+          <div className={styles.socialMedia}>
+            <a href="https://github.com/Felipewiiu" target='blank'><Github /></a>
+            <a href="https://www.instagram.com/" target='blank'><Instagran /></a>
+            <a href="https://www.linkedin.com/in/felipe-front-end/" target='blank'><Linkedin /></a>
+          </div>
+        </div>
       </div>
+
 
     </section >
   );
