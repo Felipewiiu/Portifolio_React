602a89bc8833d4f19472b6c6ff7925358240b650
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-08-12 08:50:54 -0300

    Chore: Ajustes no layoute da página home

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default function Posts() {
   return (
-    <h1 >Nada para mostrar ainda!</h1>
+    <h1 style={{marginTop: '8rem'}} >Nada para mostrar ainda!</h1>
   );
 }
