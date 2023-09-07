f6667deff3111a7edbc81023b10274036066e434
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-09-06 22:02:37 -0300

    Styles: adicionando o tema dark na página de posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,13 @@
 import styles from './posts.module.scss';
 import gif from 'assets/Finn gif 9.gif';
-
+import { useContext } from 'react';
+import { ThemeContext } from 'context/themeContext';
 
 export default function Posts() {
+  const {theme} = useContext(ThemeContext);
   return (
     <div className={styles.container}>
-      <h1 style={{ marginTop: '8rem' }} >Estamos trabalhando nisso!</h1>
+      <h1 style={theme === 'dark'? {color: '#fdfdfd'} : {color:'#2d2d2d'}} >Estamos trabalhando nisso!</h1>
       <img src={gif} alt="" />
     </div>
   );
